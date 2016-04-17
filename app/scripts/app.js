f4f51af2ee36a62322d706d278886e3689046480
tclchiam <tclchiam@gmail.com>
2016-04-17 16:54:06 -0400

    Initial commit from yo angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name babysitterKataApp
+ * @description
+ * # babysitterKataApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('babysitterKataApp', [
+    'ngAnimate',
+    'ngTouch'
+  ]);
